edc3bd4e1dd0f3db2e297c8ec9af1fdf47d380e9
Marek Guevara Braun <marek.guevara@rtbhouse.com>
2020-07-09 21:54:18 +0200

    use fpm python options instead of update-alternatives

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-update-alternatives --remove python /usr/bin/python2
-update-alternatives --install /usr/bin/python python /usr/bin/python3 10
-gem install fpm
-fpm -s python -t deb --iteration $(date +%s) --python-package-name-prefix python3 setup.py
-cp -vf *.deb /packages/$dist
+dist=${dist:-''}
 
+gem install fpm
+fpm -s python \
+    -t deb \
+    --iteration "$(date +%s)" \
+    --python-package-name-prefix python3 \
+    --python-bin python3 \
+    setup.py
+cp -vf ./*.deb "/packages/${dist}"
